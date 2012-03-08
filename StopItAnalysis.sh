61904be1b5754f64bb53e4800e34ef97b3a7e1cf
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-08 13:02:45 -0500

    modified output folder, clear existing results.

--- StopItAnalysis.sh
+++ StopItAnalysis.sh
@@ -4,27 +4,30 @@
 
 #usage: subjType: SC AC or YA
 
-
+echo 'Clearing existing results from SemPrMM/assessment/results/anti-saccade/ folder'
+cd /autofs/cluster/kuperberg/SemPrMM/assessment/results/stop-it/
+rm StopItResults_$1
 
 	if ($1 == SC) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/stop-it
 	   foreach f ( stop-SC* ) 
-		 python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f StopItResults_SC
+		 python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f /cluster/kuperberg/SemPrMM/assessment/results/stop-it/StopItResults_SC
 	   end
 	endif
 	
 	if ($1 == YA) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/stop-it	   
 	   foreach f ( stop-YA* )
-		   python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f StopItResults_YA
+		   python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f /cluster/kuperberg/SemPrMM/assessment/results/stop-it/StopItResults_YA
 	   end
 	endif 
 	
 	if ($1 == AC) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/stop-it
 	   foreach f ( stop-AC* )
-		  python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f StopItResults_AC
+		  python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f /cluster/kuperberg/SemPrMM/assessment/results/stop-it/StopItResults_AC
 	   end
 	endif
 
 
+echo 'Completed. See result in SemPrMM/assessment/results/stop-it folder'
