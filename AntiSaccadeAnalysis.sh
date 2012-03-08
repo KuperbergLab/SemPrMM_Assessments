67a6b83461c6e2a39363c37b6572b6eab1ebde77
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-08 10:22:06 -0500

    Modified output to write files in assessment/results/anti-saccade/ folder

--- AntiSaccadeAnalysis.sh
+++ AntiSaccadeAnalysis.sh
@@ -3,10 +3,10 @@
 
 # subjType: SC AC or YA
 # resultType: 1 - Short Form
-            : 2 - Long Form
+#           : 2 - Long Form
 
-echo 'Clearing existing results from assessment/AntiSaccade/subject_mod folder'
-cd /autofs/cluster/kuperberg/SemPrMM/assessment/anti-saccade/$1_mod
+echo 'Clearing existing results from SemPrMM/assessment/results/anti-saccade/ folder'
+cd /autofs/cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/
 if ($2 == 2) then
        rm longOut$1
 else
@@ -34,21 +34,21 @@ if ($2 == 1) then
 	if ($1 == SC) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/anti-saccade/SC_mod
 	   foreach f ( SC* ) 
-		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f shortOutSC $2
+		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f /cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/shortOutSC $2
 	   end
 	endif
 	
 	if ($1 == YA) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/anti-saccade/YA_mod
 	   foreach f ( YA* )
-		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f shortOutYA $2
+		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f /cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/shortOutYA $2
 	   end
 	endif 
 	
 	if ($1 == AC) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/anti-saccade/AC_mod
 	   foreach f ( AC* )
-		  python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f shortOutAC $2
+		  python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f /cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/shortOutAC $2
 	   end
 	endif
 
@@ -60,23 +60,22 @@ if ($2 == 2) then
 	if ($1 == SC) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/anti-saccade/SC_mod
 	   foreach f ( SC* ) 
-		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f longOutSC $2
+		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f /cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/longOutSC $2
 	   end
 	endif
 	
 	if ($1 == YA) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/anti-saccade/YA_mod
 	   foreach f ( YA* )
-		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f longOutYA $2
+		   python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f /cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/longOutYA $2
 	   end
 	endif 
 	
 	if ($1 == AC) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/anti-saccade/AC_mod
 	   foreach f ( AC* )
-		  python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f longOutAC $2
+		  python /cluster/kuperberg/SemPrMM/assessment/scripts/antiSaccade.py $f /cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/longOutAC $2
 	   end
 	endif
-
 endif
 
