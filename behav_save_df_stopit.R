617a4262bed53de61428d9f995dc895a39ab01aa
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-09 14:34:18 -0500

    Filename change

--- behav_save_df_stopit.R
+++ behav_save_df_stopit.R
@@ -1,11 +1,11 @@
 behav_save_df_stopit <- function(subjType){
 
 filePath <- "/cluster/kuperberg/SemPrMM/assessment/results/stop-it/"
-fileName <- paste(filePath,'StopItResults_',subjType,sep="")
+fileName <- paste(filePath,subjType,'_StopItResults',sep="")
 data <- read.table(skip=1, fileName, header=FALSE)
-stopitData.all <- data.frame(subj = factor(data$V1), cresp=data$V2, ssrt=data$V3)
+stopitData.all <- data.frame(subj = factor(data$V1), acc=data$V2, ssrt=data$V3)
 
-outFile <-paste(filePath,"R/", 'StopIt','_',subjType, '_accuracy.df', sep="")
+outFile <-paste(filePath,"R/",subjType,'_StopIt.df', sep="")
 save(stopitData.all, file=outFile)
 
 }
