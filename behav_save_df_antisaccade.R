58c84c63a87fad91a1d2b786bc3b037b30f731b6
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-09 16:13:59 -0500

    column name changed

--- behav_save_df_antisaccade.R
+++ behav_save_df_antisaccade.R
@@ -3,7 +3,7 @@ behav_save_df_antisaccade <- function(subjType){
 filePath <- "/cluster/kuperberg/SemPrMM/assessment/results/anti-saccade/"
 fileName <- paste(filePath, "R/",subjType,'_AntiSacc_long',sep="")
 data <- read.table(skip=1, fileName, header=FALSE)
-antiData.all <- data.frame(subj = factor(data$V1), condn = factor(data$V2), acc=data$V3, rt=data$V4)
+antiData.all <- data.frame(subj = factor(data$V1), cond = factor(data$V2), acc=data$V3, rt=data$V4)
 
 outFile <-paste(filePath,"R/", subjType,'_AntiSaccade_accuracy.df', sep="")
 save(antiData.all, file=outFile)
