7b305c0cf75a5d8159e07cd1d83d35495d46ac65
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-12 15:44:11 -0400

    List Prefix included

--- StopItAnalysis.sh
+++ StopItAnalysis.sh
@@ -1,33 +1,34 @@
 #!/bin/csh -f 
 
-#usage: StopItAnalysis.sh subjType
+#usage: StopItAnalysis.sh subjType listPrefix
 
 #usage: subjType: sc or ac or ya (lower case)
 
 echo 'Clearing existing results from SemPrMM/assessment/results/stopit/ folder'
 cd /autofs/cluster/kuperberg/SemPrMM/assessment/results/stop-it/
 rm $1_StopItResults
+rm $2_StopItResults
 
 	if ($1 == sc) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/stop-it
-	   foreach f ( stop-sc* ) 
-		 python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f /cluster/kuperberg/SemPrMM/assessment/results/stop-it/sc_StopItResults
+           foreach line (`cat /autofs/cluster/kuperberg/SemPrMM/assessment/scripts/function_input/$2.txt`) 
+		 python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py stop-$line.txt /cluster/kuperberg/SemPrMM/assessment/results/stop-it/$2_StopItResults
 	   end
 	   echo 'Completed. See result in SemPrMM/assessment/results/stop-it folder'
 	endif
 	
 	if ($1 == ya) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/stop-it	   
-	   foreach f ( stop-ya* )
-		   python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f /cluster/kuperberg/SemPrMM/assessment/results/stop-it/ya_StopItResults
+           foreach line (`cat /autofs/cluster/kuperberg/SemPrMM/assessment/scripts/function_input/$2.txt`)
+		   python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py stop-$line.txt /cluster/kuperberg/SemPrMM/assessment/results/stop-it/$2_StopItResults
 	   end
 	   echo 'Completed. See result in SemPrMM/assessment/results/stop-it folder'
 	endif 
 	
 	if ($1 == ac) then
 	   cd /autofs/cluster/kuperberg/SemPrMM/assessment/stop-it
-	   foreach f ( stop-ac* )
-		  python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py $f /cluster/kuperberg/SemPrMM/assessment/results/stop-it/ac_StopItResults
+           foreach line (`cat /autofs/cluster/kuperberg/SemPrMM/assessment/scripts/function_input/$2.txt`)
+		  python /cluster/kuperberg/SemPrMM/assessment/scripts/stopit.py stop-$line.txt /cluster/kuperberg/SemPrMM/assessment/results/stop-it/$2_StopItResults
 	   end
 	   echo 'Completed. See result in SemPrMM/assessment/results/stop-it folder'
 	endif
