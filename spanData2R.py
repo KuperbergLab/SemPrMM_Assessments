9aabb1b0a0e042589d5937030d2883706a063514
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-20 12:03:09 -0400

    accepts the subject type and the subjectListPrefix file as inputs to generate the longformText file for R with onely those subject data specified in the listprefic document.

--- spanData2R.py
+++ spanData2R.py
@@ -6,11 +6,12 @@ from operator import itemgetter, attrgetter
 
 # usage: python mriaccuracy.py BaleenMM or MaskedMM or AXCPT
 
-def spanData2R(subjType):
+def spanData2R(subjType, listPrefix):
         data_path = '/cluster/kuperberg/SemPrMM/assessment/'
-        inFile1 = data_path + 'span/Input/' + 'WM-SpanLog_' + str(subjType)  
-        outFile1 = data_path + 'results/span/R/' + str(subjType) + '_WM-Span_Long'     
-        print(inFile1)
+        inFile1 = data_path + 'scripts/function_input/' + str(listPrefix) + '.txt'  
+        inFile2 = data_path + 'span/Input/' + 'WM-SpanLog_' + str(subjType)  
+        outFile1 = data_path + 'results/span/R/' + str(listPrefix) + '_WM-Span_Long.txt'  
+
         
 ##     if (subjType == 'ac'):
 ##         subject_filename = data_path + 'scripts/function_inputs/ac.meg.all.txt'
@@ -18,10 +19,11 @@ def spanData2R(subjType):
 ##         subject_filename = data_path + 'scripts/function_inputs/sc.meg.all.txt'
 ##     if (subjType == 'ya'):
 ##         subject_filename = data_path + 'scripts/function_inputs/ya.meg.all.txt'
-##     subject_list = readInput.readList(subject_filename)
-##     print subject_list
+        subject_list = readInput.readList(inFile1)
+        ##print subject_list
 ##
-##     for subject in subject_list:
+        for subject in subject_list:
+            print subject
   
 #To Find the total number of rejected trials, and percentage inclusion
         #subjID = str(subjType)+ str(subject)
@@ -39,7 +41,7 @@ def spanData2R(subjType):
         else:
             print("Check the input file name")
     
-        myFile1 = open(inFile1, "r") 
+        myFile1 = open(inFile2, "r") 
         while temp:
              temp = myFile1.readline()
              temp1 = temp.strip() # not ('')
@@ -58,17 +60,19 @@ def spanData2R(subjType):
         myFile2.write("Sub:\t\tSpan\t\tNumCorrect\n")	
         for i in range(1, runCount):
                lineTemp = (dataTable1[i])
-               print(lineTemp)
-               #print(lineTemp[])
+               ##print(lineTemp)
+               #print(lineTemp[0])
                for j in range(1,span_len):
-                     myFile2.write(lineTemp[0] + '\t\t')
-                     myFile2.write(header[j] + '\t\t')
-                     myFile2.write(lineTemp[j])
-                     myFile2.write('\n')
+                     if str(lineTemp[0]) in subject_list: 
+                                               
+                          myFile2.write(lineTemp[0] + '\t\t')
+                          myFile2.write(header[j] + '\t\t')
+                          myFile2.write(lineTemp[j])
+                          myFile2.write('\n')
         print("Completed. See result in /assessment/results/span/R/ folder")
 
 if __name__ == "__main__":
    
-   spanData2R(sys.argv[1])
+   spanData2R(sys.argv[1], sys.argv[2])
 
      
