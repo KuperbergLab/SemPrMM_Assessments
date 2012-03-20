31e38a89f15617f3bdf2577d5d5b6fb8fe7ea02c
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-20 12:10:43 -0400

    Unknown Change

--- behav_save_df_stopit.R
+++ behav_save_df_stopit.R
@@ -1,11 +1,12 @@
-behav_save_df_stopit <- function(subjType){
+behav_save_df_stopit <- function(subjType, listPrefix){
 
 filePath <- "/cluster/kuperberg/SemPrMM/assessment/results/stop-it/"
-fileName <- paste(filePath,subjType,'_StopItResults',sep="")
-data <- read.table(skip=1, fileName, header=FALSE)
+fileName1 <- paste(filePath,listPrefix,'_StopItResults',sep="")
+data <- read.table(skip=1, fileName1, header=FALSE)
 stopitData.all <- data.frame(subj = factor(data$V1), acc=data$V2, ssrt=data$V3)
 
-outFile <-paste(filePath,"R/",subjType,'_StopIt.df', sep="")
+
+outFile <-paste(filePath,"R/",listPrefix,'_StopIt.df', sep="")
 save(stopitData.all, file=outFile)
 
 }
