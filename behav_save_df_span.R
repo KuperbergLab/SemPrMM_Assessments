d0cd9ef0c75426e551ecf8ff1d28146965609888
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-20 12:04:15 -0400

    Included listPrefix as input tag

--- behav_save_df_span.R
+++ behav_save_df_span.R
@@ -1,12 +1,12 @@
-behav_save_df_span <- function(subjType){
+behav_save_df_span <- function(subjType, listPrefix){
 
 filePath <- "/cluster/kuperberg/SemPrMM/assessment/results/span/R/"
-fileName <- paste(filePath,subjType,'_WM-Span_Long',sep="")
+fileName <- paste(filePath,listPrefix,'_WM-Span_Long.txt',sep="")
 
 data <- read.table(skip=1, fileName, header=FALSE)
 spanData.all <- data.frame(subj = factor(data$V1), spType=data$V2, numCor=data$V3)
 
-outFile <-paste(filePath,subjType,'_WM-SpanLog_', '.df', sep="")
+outFile <-paste(filePath,listPrefix,'_WM-SpanLog', '.df', sep="")
 save(spanData.all, file=outFile)
 
 }
